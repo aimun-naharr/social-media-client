d20060f9273e3447962290df5ac4c95c809d8164
aimunNaharr <aimunnaharr@gmail.com>
2022-06-25 15:28:03 +0600

    profile structure created

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css'
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="profileside">Profile</div>
+      <div className="postside">Post</div>
+      <div className="rightside">Right</div>
+    </div>
+  );
+};
+
+export default Home;
